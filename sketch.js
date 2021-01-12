291a3ff113469aa86f35199d2e910aff969bd4bb
jskor013 <77273827+jskor013@users.noreply.github.com>
2021-01-12 22:17:37 +0900

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let img, classifier;
+
+function setup() {
+  createCanvas(400, 400);
+  img = loadImage('image1.jpg');
+  classifier = ml5.imageClassifier('MobileNet', modelReady);
+}
+
+function modelReady(){
+  console.log('MobileNet 모델 불러오기 완료');
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(err, results){
+  if(err){
+    console.error(err);
+    return;
+  }
+  console.log(results);
+}
